7d3965de9d57a3b239df137af7e028613fd0aeec
stepniaczky <m.stepniak@yahoo.com>
2023-04-02 23:57:19 +0200

    [CHORE] code cleanup

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -2,13 +2,10 @@
 export default {
   content: [
     "./src/**/*.{html,js,jsx,ts,tsx}",
-    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require("flowbite/plugin")
-  ],
-}
-
+  plugins: [require("flowbite/plugin")],
+};
