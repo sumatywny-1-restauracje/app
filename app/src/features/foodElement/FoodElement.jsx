0b3ebfe4ec17267ddb5b63f3914668453a7121f2
Mikołaj Stępniak <m.stepniak@yahoo.com>
2023-04-10 01:13:57 +0200

    [REFACTOR] Converting a Food component into a general-purpose one (#7)

--- app/src/features/foodElement/FoodElement.jsx
+++ app/src/features/foodElement/FoodElement.jsx
@@ -1,6 +1,6 @@
 import { Rating } from "flowbite-react";
 
-const FoodElement = () => {
+const FoodElement = ({ product }) => {
   return (
     <div className="md: relative mx-auto flex h-[17rem] w-52 items-end justify-start sm:h-72 sm:w-52 md:h-80 md:w-60 lg:h-[22rem] lg:w-60 xl:h-[23rem] xl:w-64">
       <div
@@ -8,8 +8,8 @@ const FoodElement = () => {
         className="absolute right-0 top-0 overflow-hidden rounded-full border-rose-400"
       >
         <img
-          src="https://images.pexels.com/photos/12034622/pexels-photo-12034622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-          alt="Chicken Burger"
+          src={product?.image}
+          alt={product?.name}
           className="h-32 w-32 object-fill md:h-44 md:w-44 xl:h-52 xl:w-52"
         />
       </div>
@@ -17,21 +17,21 @@ const FoodElement = () => {
         <div className="flex w-full flex-col gap-4 pb-5">
           <div>
             <h4 className="text-md font-extrabold md:text-lg xl:text-xl">
-              Chicken Burger
+              {product?.name}
             </h4>
             <Rating className="mt-1">
-              <Rating.Star />
-              <Rating.Star />
-              <Rating.Star />
-              <Rating.Star />
-              <Rating.Star />
+              <Rating.Star filled={product?.rating > 0 && true} />
+              <Rating.Star filled={product?.rating > 1 && true} />
+              <Rating.Star filled={product?.rating > 2 && true} />
+              <Rating.Star filled={product?.rating > 3 && true} />
+              <Rating.Star filled={product?.rating > 4 && true} />
               <p className="ml-2 text-xs font-medium text-gray-500 dark:text-gray-400 lg:text-sm">
-                (160)
+                ({product?.numberOfRatings})
               </p>
             </Rating>
           </div>
           <div className="flex items-center justify-between">
-            <span className="text-xl font-bold">$3.50</span>
+            <span className="text-xl font-bold">${product?.price}</span>
             <button className="rounded-2xl bg-rose-400 px-2 py-1 text-xs font-semibold text-white hover:bg-rose-500 lg:px-3 xl:text-base xl:font-semibold">
               Buy Now
             </button>
