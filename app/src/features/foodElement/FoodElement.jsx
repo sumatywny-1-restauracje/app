c7d3ada809a9af92176431983f03b10c7a9107e9
stepniaczky <m.stepniak@yahoo.com>
2023-04-10 23:49:08 +0200

    [FIX] Correcting spacing in Home Page Teaser & Food Element

--- app/src/features/foodElement/FoodElement.jsx
+++ app/src/features/foodElement/FoodElement.jsx
@@ -2,11 +2,8 @@ import { Rating } from "flowbite-react";
 
 const FoodElement = ({ product }) => {
   return (
-    <div className="md: relative mx-auto flex h-[17rem] w-52 items-end justify-start sm:h-72 sm:w-52 md:h-80 md:w-60 lg:h-[22rem] lg:w-60 xl:h-[23rem] xl:w-64">
-      <div
-        style={{ fontSize: "15rem", borderWidth: "12px" }}
-        className="absolute right-0 top-0 overflow-hidden rounded-full border-rose-400"
-      >
+    <div className="relative mx-auto flex h-[17rem] w-52 items-end justify-start sm:h-[17rem] sm:w-52 md:h-80 md:w-60 lg:h-[20rem] lg:w-60 xl:h-[23rem] xl:w-64">
+      <div className="absolute right-0 top-0 overflow-hidden rounded-full border-[12px] border-rose-400 text-[15rem]">
         <img
           src={product?.image}
           alt={product?.name}
@@ -16,7 +13,7 @@ const FoodElement = ({ product }) => {
       <div className="flex h-52 w-44 items-end rounded-2xl bg-orange-100 px-5 text-lg text-gray-700 sm:h-48 sm:w-44 md:h-60 md:w-48 xl:h-80 xl:w-56">
         <div className="flex w-full flex-col gap-4 pb-5">
           <div>
-            <h4 className="text-md font-extrabold md:text-lg xl:text-xl">
+            <h4 className="text-base font-extrabold md:text-lg xl:text-xl">
               {product?.name}
             </h4>
             <Rating className="mt-1">
@@ -25,14 +22,14 @@ const FoodElement = ({ product }) => {
               <Rating.Star filled={product?.rating > 2 && true} />
               <Rating.Star filled={product?.rating > 3 && true} />
               <Rating.Star filled={product?.rating > 4 && true} />
-              <p className="ml-2 text-xs font-medium text-gray-500 dark:text-gray-400 lg:text-sm">
+              <p className="ml-2 text-xs font-medium text-gray-500 lg:text-sm">
                 ({product?.numberOfRatings})
               </p>
             </Rating>
           </div>
           <div className="flex items-center justify-between">
             <span className="text-xl font-bold">${product?.price}</span>
-            <button className="rounded-2xl bg-rose-400 px-2 py-1 text-xs font-semibold text-white hover:bg-rose-500 lg:px-3 xl:text-base xl:font-semibold">
+            <button className="rounded-2xl bg-rose-400 px-2 py-1 text-xs font-semibold text-white hover:bg-rose-500 lg:px-3 xl:text-base">
               Buy Now
             </button>
           </div>
