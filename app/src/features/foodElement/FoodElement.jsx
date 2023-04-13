66410fe6e70da2fdc639ef37f13e92029e499525
stepniaczky <m.stepniak@yahoo.com>
2023-04-13 16:42:34 +0200

    [REFACTOR] Changing mobile view of Food Element

--- app/src/features/foodElement/FoodElement.jsx
+++ app/src/features/foodElement/FoodElement.jsx
@@ -2,12 +2,12 @@ import { Rating } from "flowbite-react";
 
 const FoodElement = ({ product }) => {
   return (
-    <div className="relative mx-auto flex h-[17rem] w-52 items-end justify-start sm:h-[17rem] sm:w-52 md:h-80 md:w-60 lg:h-[20rem] lg:w-60 xl:h-[23rem] xl:w-64">
-      <div className="absolute right-0 top-0 overflow-hidden rounded-full border-[12px] border-rose-400 text-[15rem]">
+    <div className="relative mx-auto flex h-[18.5rem] w-44 items-end justify-start sm:h-[18.5rem] sm:w-52 md:h-80 md:w-60 lg:h-[20rem] lg:w-60 xl:h-[23rem] xl:w-64">
+      <div className="absolute right-0 top-0 overflow-hidden rounded-full border-[12px] border-rose-400 text-[15rem] max-sm:w-full">
         <img
           src={product?.image}
           alt={product?.name}
-          className="h-32 w-32 object-fill md:h-44 md:w-44 xl:h-52 xl:w-52"
+          className="h-[152px] w-[152px] object-fill sm:h-[10rem] sm:w-[10rem] md:h-44 md:w-44 xl:h-52 xl:w-52"
         />
       </div>
       <div className="flex h-52 w-44 items-end rounded-2xl bg-orange-100 px-5 text-lg text-gray-700 sm:h-48 sm:w-44 md:h-60 md:w-48 xl:h-80 xl:w-56">
