4b0300b50b7d49528b1bb4f0937f7ddaea0b5ff4
stepniaczky <m.stepniak@yahoo.com>
2023-04-24 22:44:08 +0200

    [CHORE] Update config files

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -1,9 +1,12 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 } satisfies Config;
