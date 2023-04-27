64ed31ea88448d822fcd0a31e7cbe6ba7cdf8cf2
stepniaczky <m.stepniak@yahoo.com>
2023-04-27 20:32:09 +0200

    [FIX] Fix height of special offer elements

--- app/components/home/SpecialOffers.tsx
+++ app/components/home/SpecialOffers.tsx
@@ -10,10 +10,10 @@ const SpecialOffers = ({ specialOffers }: SpecialOffersProps) => {
       <h2 className="w-full bg-clip-text text-3xl font-extrabold text-gray-700 max-md:text-center sm:text-4xl md:text-5xl">
         New <span className="text-rose-400">Special</span> Offers
       </h2>
-      <div className="grid max-h-[27rem] grid-cols-1 gap-4 sm:grid-cols-2">
+      <div className="grid h-full max-h-[27rem] grid-cols-1 gap-4 sm:grid-cols-2">
         <div className="relative row-span-2 overflow-hidden rounded-xl">
           <span className="absolute left-3 top-3 w-max rounded-lg bg-gray-500 bg-opacity-70 p-2 text-base font-bold text-white md:text-3xl lg:text-4xl">
-            {specialOffers[0].image.value} Discount
+            {specialOffers[0].value} Discount
           </span>
           <img
             src={specialOffers[0].image.src}
@@ -23,7 +23,7 @@ const SpecialOffers = ({ specialOffers }: SpecialOffersProps) => {
         </div>
         <div className="relative overflow-hidden rounded-xl">
           <span className="absolute right-3 top-3 w-max rounded-lg bg-gray-500 bg-opacity-70 p-2 text-base font-bold text-white md:text-3xl lg:text-4xl">
-            25% Discount
+            {specialOffers[1].value} Discount
           </span>
           <img
             src={specialOffers[1].image.src}
@@ -33,7 +33,7 @@ const SpecialOffers = ({ specialOffers }: SpecialOffersProps) => {
         </div>
         <div className="relative overflow-hidden rounded-xl">
           <span className="absolute bottom-3 right-3 w-max rounded-lg bg-gray-500 bg-opacity-70 p-2 text-base font-bold text-white md:text-3xl lg:text-4xl">
-            25% Discount
+            {specialOffers[2].value} Discount
           </span>
           <img
             src={specialOffers[2].image.src}
