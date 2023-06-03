017b50f74ee5d8f594b3d26abe8df31fd15dbc9c
Mikołaj Stępniak <m.stepniak@yahoo.com>
2023-06-03 13:25:26 +0200

    [FEAT] Make order route (#30)
    * [CHORE] Update .env.example
    
    * [FEAT] Work in progress
    
    * [FEAT] Work in progress
    
    * [FIX] Add new line at the end of the file

--- app/models/basket.ts
+++ app/models/basket.ts
@@ -3,7 +3,8 @@ import type { BasketItem, User } from "types";
 const BASKET: Array<BasketItem> = [
   {
     id: 1,
-    price: 10.0,
+    name: "Pizza",
+    price: 4.2,
     quantity: 1,
     image: {
       src: "https://images.pexels.com/photos/15126955/pexels-photo-15126955.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
@@ -12,47 +13,32 @@ const BASKET: Array<BasketItem> = [
   },
   {
     id: 2,
-    price: 5.0,
+    name: "Chicken Burger",
+    price: 3.5,
     quantity: 5,
     image: {
       src: "https://images.pexels.com/photos/12034622/pexels-photo-12034622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-      alt: "Hamburger",
+      alt: "Chicken Burger",
     },
   },
   {
     id: 3,
+    name: "Chicken Fry",
     price: 5.0,
     quantity: 5,
     image: {
-      src: "https://images.pexels.com/photos/12034622/pexels-photo-12034622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-      alt: "Hamburger",
+      src: "https://images.pexels.com/photos/10361459/pexels-photo-10361459.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      alt: "Chicken Fry",
     },
   },
   {
     id: 4,
-    price: 5.0,
+    name: "Grill Sandwich",
+    price: 4.8,
     quantity: 5,
     image: {
-      src: "https://images.pexels.com/photos/12034622/pexels-photo-12034622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-      alt: "Hamburger",
-    },
-  },
-  {
-    id: 5,
-    price: 5.0,
-    quantity: 5,
-    image: {
-      src: "https://images.pexels.com/photos/12034622/pexels-photo-12034622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-      alt: "Hamburger",
-    },
-  },
-  {
-    id: 6,
-    price: 5.0,
-    quantity: 5,
-    image: {
-      src: "https://images.pexels.com/photos/12034622/pexels-photo-12034622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-      alt: "Hamburger",
+      src: "https://images.pexels.com/photos/5634630/pexels-photo-5634630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      alt: "Grill Sandwich",
     },
   },
 ];
