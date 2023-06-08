8541cda416bdc64017ea5c7b827eae684d054a9c
Jacek Nowak <jaceknowak226@gmail.com>
2023-06-08 16:20:03 +0200

    XDDDDDD

--- app/routes/employee.tsx
+++ app/routes/employee.tsx
@@ -4,8 +4,21 @@ import type { V2_MetaFunction } from "@remix-run/react";
 import { useLoaderData } from "@remix-run/react";
 import clsx from "clsx";
 import dayjs from "dayjs";
+import { useState } from "react";
 import { getClientsOrdersByRestaurant } from "~/models/order.server";
 import { getUserInformation } from "~/models/user.server";
+import { api } from "~/utils/api";
+
+const statuses: any = {
+  EMPLOYEE: [
+    "NOT_APPROVED",
+    "APPROVED",
+    "IN_PROGRESS",
+    "DELIVERY",
+    "EMPLOYEE_CANCELLED",
+  ],
+  DELIVERY: ["DELIVERY", "COMPLETED"],
+};
 
 const ordersFake = [...Array(10).keys()].map((key) => ({
   id: key,
@@ -66,7 +79,7 @@ const InfoItem = ({ big, label, value }: any) => {
   );
 };
 
-const OrderItem = ({ order }: any) => {
+const OrderItem = ({ order, onChange, role }: any) => {
   const date = dayjs(order.createdAt).format("DD.MM.YYYY");
   const { street, houseNumber, apartment } = order.address;
   const addressLine = `${street} ${houseNumber}${
@@ -74,7 +87,7 @@ const OrderItem = ({ order }: any) => {
   }`;
 
   return (
-    <div className="flex gap-8 rounded-2xl bg-orange-200 px-10 py-6">
+    <div className="flex items-start justify-between gap-8 rounded-2xl bg-orange-200 px-10 py-6">
       <div className="flex flex-col gap-1">
         <div className="mb-2 flex items-center gap-3">
           <p className="text-lg font-bold">{`Zamówienie #${order.id}`}</p>
@@ -90,6 +103,18 @@ const OrderItem = ({ order }: any) => {
         <InfoItem label="Miasto" value={order.address.city} />
         <InfoItem label="Kraj" value={order.address.country} />
       </div>
+      <select
+        className="rounded-lg border-4 border-rose-400 bg-orange-100 bg-transparent p-2 font-bold focus:border-rose-500 focus:ring-0"
+        defaultValue={order.status}
+        name="status"
+        onChange={onChange}
+      >
+        {statuses[role].map((status: any) => (
+          <option key={status} className="bg-rose-300" value={status}>
+            {status}
+          </option>
+        ))}
+      </select>
     </div>
   );
 };
@@ -97,11 +122,18 @@ const OrderItem = ({ order }: any) => {
 export default function EmployeePanelRoute() {
   const {
     user: userRes,
-    orders: { orders },
+    // orders: { orders },
   } = useLoaderData();
   const { userData: user, employeeData: employee } = userRes;
+  const [orders, setOrders] = useState(ordersFake);
 
-  console.log(orders);
+  const handleChange = async (id: any, value: any) => {
+    try {
+      await api.patch(`/orders/${id}/${value}`);
+      const res = await api.get(`/order/restaurant/${employee.restaurantId}`);
+      setOrders(res.data);
+    } catch {}
+  };
 
   return (
     <div className="mb-12 mt-8 flex w-5/6 max-w-screen-lg flex-col gap-9 rounded-xl border-rose-300 bg-orange-100 p-4 md:w-4/6">
@@ -123,9 +155,27 @@ export default function EmployeePanelRoute() {
       <div className="h-full min-h-[600px] w-full break-all">
         <div className="flex flex-col gap-6">
           {!!orders.length &&
-            orders.map((order: any) => (
-              <OrderItem key={order.id} order={order} />
-            ))}
+            orders.map((order: any) => {
+              if (user.userRole === "DELIVERY" && order.status !== "DELIVERY") {
+                return null;
+              }
+
+              if (
+                user.userRole === "EMPLOYEE" &&
+                (order.status === "COMPLETED" || order.status === "DELIVERY")
+              ) {
+                return null;
+              }
+
+              return (
+                <OrderItem
+                  key={order.id}
+                  role={user.userRole}
+                  order={order}
+                  onChange={(e: any) => handleChange(order.id, e.target.value)}
+                />
+              );
+            })}
           {!orders.length && (
             <p className="px-6 text-gray-500">
               Brak zamówień dla danej restauracji
